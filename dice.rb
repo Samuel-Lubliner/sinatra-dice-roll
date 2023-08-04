dc8c49852c7ed6354e390ad06f898f1ed2adaa95
Samuel Lubliner <74507133+Samuel-Lubliner@users.noreply.github.com>
2023-08-04 03:49:09 +0000

    dynamic

--- dice.rb
+++ dice.rb
@@ -20,42 +20,15 @@ get("/giraffe") do
   "Hopefully this shows up without having to restart the server 🤞🏾"
 end
 
-get("/dice/2/6") do
-  first_die = rand(1..6)
-	second_die = rand(1..6)
-  sum = first_die + second_die
-	
-	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-	erb(:two_six)
-end
+get "/dice/:num_dice/:num_sides" do
+  @rolls = []
+  num_dice = params[:num_dice].to_i
+  num_sides = params[:num_sides].to_i
 
-get("/dice/2/10") do
-  first_die = rand(1..10)
-	second_die = rand(1..10)
-  sum = first_die + second_die
-	
-	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  erb(:two_ten)
-end
+  num_dice.times do
+    die = rand(1..num_sides)
+    @rolls.push(die)
+  end
 
-get("/dice/1/20") do
-  die = rand(1..20)
-	
-	@outcome = "You rolled a #{die}."
-	
-	erb(:one_twenty)
-end
-
-get("/dice/5/4") do
-  first_die = rand(1..4)
-	second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die
-	
-	@outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
-	
-  erb(:five_four)
+  erb :dice
 end
